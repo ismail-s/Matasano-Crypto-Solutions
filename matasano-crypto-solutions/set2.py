ae3d5f775e2b5aad8e7461f86fc25cf032b68238
Ismail <astroman3d@gmail.com>
2016-11-22 13:09:26 +0000

    Fix pad_with_pkcs7 func

--- matasano-crypto-solutions/set2.py
+++ matasano-crypto-solutions/set2.py
@@ -9,7 +9,7 @@ from utils import (
 
 def pad_with_pkcs7(message: bytes, block_length: int) -> bytes:
     if len(message) >= block_length:
-        amount_to_pad = len(message) % block_length
+        amount_to_pad = block_length - (len(message) % block_length)
     else:
         amount_to_pad = block_length - len(message)
     res = message + (bytes([amount_to_pad]) * amount_to_pad)
