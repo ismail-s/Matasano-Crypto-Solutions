5b87a554bd4f59d969ce48343569f3ab258ad112
Ismail <astroman3d@gmail.com>
2016-11-22 13:10:52 +0000

    Solve challenge 11

--- matasano-crypto-solutions/set2.py
+++ matasano-crypto-solutions/set2.py
@@ -1,8 +1,11 @@
+import os, random
+from collections import Counter
 from utils import (
     base64_to_bytes,
     aes_ecb_decode,
     aes_ecb_encode,
     get_file,
+    text_to_bytes,
     split_into_groups,
     xor)
 
@@ -37,6 +40,34 @@ def aes_cbc_encode(plaintext: bytes, password: bytes, iv: bytes) -> bytes:
     return b''.join(res)
 
 
+def random_bytes(length: int) -> bytes:
+    return os.urandom(length)
+
+
+def random_aes_key() -> bytes:
+    return random_bytes(16)
+
+
+def encryption_oracle(plaintext: bytes) -> bytes:
+    before = random_bytes(random.randint(5, 10))
+    after = random_bytes(random.randint(5, 10))
+    to_encrypt = pad_with_pkcs7(before + plaintext + after, 16)
+    enc_func = random.choice((aes_ecb_encode, aes_cbc_encode))
+    if (enc_func == aes_cbc_encode):
+        print('oracle encrypted plaintext with cbc')
+        return enc_func(to_encrypt, random_aes_key(), random_aes_key())
+    else:
+        print('oracle encrypted plaintext with aes')
+        return enc_func(to_encrypt, random_aes_key())
+
+
+def check_if_aes_or_cbc(text: bytes) -> str:
+    counts = Counter(split_into_groups(ciphertext, 16))
+    if counts.most_common(1)[0][1] > 1:
+        return 'aes'
+    return 'cbc'
+
+
 print('Set 2')
 print('Challenge 9')
 res9 = pad_with_pkcs7(b'YELLOW SUBMARINE', 20)
@@ -53,3 +84,8 @@ print(res10)
 # Check that encrypting is the opposite of decrypting
 test_ciphertext10 = aes_cbc_encode(res10.encode('ascii'), password10, iv)
 assert test_ciphertext10 == ciphertext10
+
+print('Challenge 11')
+plaintext = text_to_bytes(res10)
+ciphertext = encryption_oracle(plaintext)
+print(check_if_aes_or_cbc(ciphertext))
