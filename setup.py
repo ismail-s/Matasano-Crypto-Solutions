54de40dd7f51c24e375552d7599600e4f40e37d8
Ismail <astroman3d@gmail.com>
2016-12-05 13:56:10 +0000

    Place pytest last in tests_require

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setuptools.setup(name='matasano-crypto-solutions',
                  py_modules=['matasano_crypto_solutions'],
                  install_requires=open('requirements.txt').read().split(),
                  setup_requires=['pytest-runner'],
-                 tests_require=['pytest', 'pytest-flake8', 'pytest-cov'],
+                 tests_require=['pytest-flake8', 'pytest-cov', 'pytest'],
                  license='MIT License',
                  zip_safe=True)
